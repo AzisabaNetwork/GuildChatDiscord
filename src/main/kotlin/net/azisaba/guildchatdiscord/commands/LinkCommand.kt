cf37bc29163f35bb25384ad103b9bc3064cf7e07
acrylic-style <me@acrylicstyle.xyz>
2023-01-03 14:24:29 +0900

    :bookmark: 2.1.2 - Disallow duplicate links

--- src/main/kotlin/net/azisaba/guildchatdiscord/commands/LinkCommand.kt
+++ src/main/kotlin/net/azisaba/guildchatdiscord/commands/LinkCommand.kt
@@ -1,6 +1,6 @@
 package net.azisaba.guildchatdiscord.commands
 
-import dev.kord.core.behavior.interaction.respondEphemeral
+import dev.kord.core.behavior.interaction.response.respond
 import dev.kord.core.entity.interaction.ApplicationCommandInteraction
 import dev.kord.rest.builder.interaction.GlobalMultiApplicationCommandBuilder
 import dev.kord.rest.builder.interaction.string
@@ -9,7 +9,24 @@ import net.azisaba.guildchatdiscord.util.optString
 
 object LinkCommand : CommandHandler {
     override suspend fun handle0(interaction: ApplicationCommandInteraction) {
+        val defer = interaction.deferEphemeralResponse()
         val code = interaction.optString("code")!!
+        val linked = DatabaseManager.query("SELECT `minecraft_uuid`, `minecraft_name` FROM `users` WHERE `discord_id` = ?") { stmt ->
+            stmt.setString(1, interaction.user.id.toString())
+            stmt.executeQuery().use { rs ->
+                if (rs.next()) {
+                    Pair(rs.getString("minecraft_uuid"), rs.getString("minecraft_name"))
+                } else {
+                    null
+                }
+            }
+        }
+        if (linked != null) {
+            defer.respond {
+                content = "このDiscordアカウントはすでに${linked.second} (`${linked.first}`)と連携済みです。\n連携を解除するには`/unlink`を入力してください。"
+            }
+            return
+        }
         val uuidName = DatabaseManager.query("SELECT `minecraft_uuid`, `minecraft_name` FROM `users` WHERE `link_code` = ?") { stmt ->
             stmt.setString(1, code)
             stmt.executeQuery().use { rs ->
@@ -21,7 +38,7 @@ object LinkCommand : CommandHandler {
             }
         }
         if (uuidName == null) {
-            interaction.respondEphemeral { content = "コードが無効です。" }
+            defer.respond { content = "コードが無効です。" }
             return
         }
         val discordId = DatabaseManager.query("SELECT `discord_id` FROM `users` WHERE `link_code` = ?") { stmt ->
@@ -36,7 +53,7 @@ object LinkCommand : CommandHandler {
         }
         if (discordId != -1L) {
             // already linked
-            interaction.respondEphemeral { content = "コードが無効です。" }
+            defer.respond { content = "コードが無効です。" }
             return
         }
         val (uuid, name) = uuidName
@@ -46,7 +63,7 @@ object LinkCommand : CommandHandler {
             stmt.executeUpdate()
         }
         DatabaseManager.getMinecraftUUIDByDiscordId.forget(interaction.user.id.value.toLong())
-        interaction.respondEphemeral { content = "`$name` (`$uuid`)と連携しました。" }
+        defer.respond { content = "`$name` (`$uuid`)と連携しました。" }
     }
 
     override fun register(builder: GlobalMultiApplicationCommandBuilder) {
