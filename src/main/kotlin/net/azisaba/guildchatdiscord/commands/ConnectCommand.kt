69db60487e390cae8d419340382d4682c1f674b5
sysnote8main <sysnote8main@gmail.com>
2026-04-09 23:50:36 +0900

    feat: show awaiting message

--- src/main/kotlin/net/azisaba/guildchatdiscord/commands/ConnectCommand.kt
+++ src/main/kotlin/net/azisaba/guildchatdiscord/commands/ConnectCommand.kt
@@ -2,7 +2,8 @@ package net.azisaba.guildchatdiscord.commands
 
 import dev.kord.common.entity.Permission
 import dev.kord.common.entity.Permissions
-import dev.kord.core.behavior.interaction.response.respond
+import dev.kord.core.behavior.interaction.respondPublic
+import dev.kord.core.behavior.interaction.response.edit
 import dev.kord.core.entity.channel.TopGuildChannel
 import dev.kord.core.entity.interaction.ApplicationCommandInteraction
 import dev.kord.rest.builder.interaction.GlobalMultiApplicationCommandBuilder
@@ -12,66 +13,82 @@ import net.azisaba.guildchatdiscord.getMinecraftIdName
 import net.azisaba.guildchatdiscord.util.DatabaseManager
 import net.azisaba.guildchatdiscord.util.optString
 import net.azisaba.interchat.api.user.User
-import java.util.UUID
 
 object ConnectCommand : CommandHandler {
     override suspend fun handle0(interaction: ApplicationCommandInteraction) {
-        val defer = interaction.deferPublicResponse()
+        val defer = interaction.respondPublic { content = ":construction: 現在接続中..." }
         val channel = interaction.channel.asChannel() as TopGuildChannel
         if (!channel.getEffectivePermissions(interaction.kord.selfId).contains(Permission.ManageWebhooks)) {
-            defer.respond { content = "BotがこのチャンネルにWebhookを作成する権限がありません。" }
+            defer.edit { content = "BotがこのチャンネルにWebhookを作成する権限がありません。" }
             return
         }
-        val (minecraftUuid) = interaction.user.getMinecraftIdName() ?: run {
-            defer.respond { content = "Minecraftアカウントが連携されていません。" }
-            return
-        }
+        val (minecraftUuid) =
+            interaction.user.getMinecraftIdName() ?: run {
+                defer.edit { content = "Minecraftアカウントが連携されていません。" }
+                return
+            }
         val user: User = InterChatDiscord.userManager.fetchUser(minecraftUuid).join()
         val guildName = interaction.optString("guild")!!
-        val guild = InterChatDiscord.guildManager.fetchGuildByName(guildName).exceptionally { null }.join()
+        val guild =
+            InterChatDiscord.guildManager
+                .fetchGuildByName(guildName)
+                .exceptionally { null }
+                .join()
         if (guild == null || guild.deleted()) {
-            defer.respond { content = "ギルド`$guildName`に参加していません。" }
+            defer.edit { content = "ギルド`$guildName`に参加していません。" }
             return
         }
         if (guild.getMember(user).exceptionally { null }.join() == null) {
-            defer.respond { content = "ギルド`$guildName`に参加していません。" }
+            defer.edit { content = "ギルド`$guildName`に参加していません。" }
             return
         }
-        val linkedGuildId = DatabaseManager.query("SELECT `guild_id` FROM `channels` WHERE `channel_id` = ?") {
-            it.setLong(1, channel.id.value.toLong())
-            it.executeQuery().use { rs ->
-                if (rs.next()) {
-                    rs.getLong("guild_id")
-                } else {
-                    -1L
+        val linkedGuildId =
+            DatabaseManager.query("SELECT `guild_id` FROM `channels` WHERE `channel_id` = ?") {
+                it.setLong(1, channel.id.value.toLong())
+                it.executeQuery().use { rs ->
+                    if (rs.next()) {
+                        rs.getLong("guild_id")
+                    } else {
+                        -1L
+                    }
                 }
             }
-        }
         if (linkedGuildId != -1L) {
-            val linkedGuild = InterChatDiscord.guildManager.fetchGuildById(linkedGuildId).exceptionally { null }.join()
+            val linkedGuild =
+                InterChatDiscord.guildManager
+                    .fetchGuildById(linkedGuildId)
+                    .exceptionally { null }
+                    .join()
             if (linkedGuild != null && !linkedGuild.deleted()) {
-                defer.respond {
+                defer.edit {
                     content = "このチャンネルは既にギルド`${linkedGuild.name()}`と連携されています。\n`/unconnect`で連携を解除できます。"
                 }
             } else {
-                defer.respond { content = "`/unconnect`を実行して連携を解除してからもう一度お試しください。" }
+                defer.edit { content = "`/unconnect`を実行して連携を解除してからもう一度お試しください。" }
             }
             return
         }
-        val webhook = channel.kord.rest.webhook.createWebhook(channel.id, "ギルドチャット (${guild.name()})") {
-            reason = "/connect command from ${interaction.user.tag} (${interaction.user.id})"
-        }
-        DatabaseManager.query("INSERT INTO `channels` (`guild_id`, `channel_id`, `webhook_id`, `webhook_token`, `created_user_id`) VALUES (?, ?, ?, ?, ?)") {
+        val webhook =
+            channel.kord.rest.webhook.createWebhook(channel.id, "ギルドチャット (${guild.name()})") {
+                reason = "/connect command from ${interaction.user.tag} (${interaction.user.id})"
+            }
+        DatabaseManager.query(
+            "INSERT INTO `channels` (`guild_id`, `channel_id`, `webhook_id`, `webhook_token`, `created_user_id`) VALUES (?, ?, ?, ?, ?)",
+        ) {
             it.setString(1, guild.id().toString())
             it.setLong(2, channel.id.value.toLong())
             it.setLong(3, webhook.id.value.toLong())
             it.setString(4, webhook.token.value)
-            it.setLong(5, interaction.user.id.value.toLong())
+            it.setLong(
+                5,
+                interaction.user.id.value
+                    .toLong(),
+            )
             it.executeUpdate()
         }
         DatabaseManager.getGuildIdByChannelId.forget(channel.id.value.toLong())
         DatabaseManager.getWebhooksByGuildId.forget(guild.id())
-        defer.respond { content = "ギルドチャットを`${guild.name()}`と連携しました。\nメッセージが受信できるようになるまで最大60秒程度かかります。" }
+        defer.edit { content = "ギルドチャットを`${guild.name()}`と連携しました。\nメッセージが受信できるようになるまで最大60秒程度かかります。" }
     }
 
     override fun register(builder: GlobalMultiApplicationCommandBuilder) {
--- src/main/kotlin/net/azisaba/guildchatdiscord/commands/LinkCommand.kt
+++ src/main/kotlin/net/azisaba/guildchatdiscord/commands/LinkCommand.kt
@@ -1,6 +1,7 @@
 package net.azisaba.guildchatdiscord.commands
 
-import dev.kord.core.behavior.interaction.response.respond
+import dev.kord.core.behavior.interaction.respondEphemeral
+import dev.kord.core.behavior.interaction.response.edit
 import dev.kord.core.entity.interaction.ApplicationCommandInteraction
 import dev.kord.rest.builder.interaction.GlobalMultiApplicationCommandBuilder
 import dev.kord.rest.builder.interaction.string
@@ -9,61 +10,71 @@ import net.azisaba.guildchatdiscord.util.optString
 
 object LinkCommand : CommandHandler {
     override suspend fun handle0(interaction: ApplicationCommandInteraction) {
-        val defer = interaction.deferEphemeralResponse()
+        val defer = interaction.respondEphemeral { content = ":construction: 連携中..." }
         val code = interaction.optString("code")!!
-        val linked = DatabaseManager.query("SELECT `minecraft_uuid`, `minecraft_name` FROM `users` WHERE `discord_id` = ?") { stmt ->
-            stmt.setString(1, interaction.user.id.toString())
-            stmt.executeQuery().use { rs ->
-                if (rs.next()) {
-                    Pair(rs.getString("minecraft_uuid"), rs.getString("minecraft_name"))
-                } else {
-                    null
+        val linked =
+            DatabaseManager.query("SELECT `minecraft_uuid`, `minecraft_name` FROM `users` WHERE `discord_id` = ?") { stmt ->
+                stmt.setString(1, interaction.user.id.toString())
+                stmt.executeQuery().use { rs ->
+                    if (rs.next()) {
+                        Pair(rs.getString("minecraft_uuid"), rs.getString("minecraft_name"))
+                    } else {
+                        null
+                    }
                 }
             }
-        }
         if (linked != null) {
-            defer.respond {
+            defer.edit {
                 content = "このDiscordアカウントはすでに${linked.second} (`${linked.first}`)と連携済みです。\n連携を解除するには`/unlink`を入力してください。"
             }
             return
         }
-        val uuidName = DatabaseManager.query("SELECT `minecraft_uuid`, `minecraft_name` FROM `users` WHERE `link_code` = ?") { stmt ->
-            stmt.setString(1, code)
-            stmt.executeQuery().use { rs ->
-                if (rs.next()) {
-                    Pair(rs.getString("minecraft_uuid"), rs.getString("minecraft_name"))
-                } else {
-                    null
+        val uuidName =
+            DatabaseManager.query("SELECT `minecraft_uuid`, `minecraft_name` FROM `users` WHERE `link_code` = ?") { stmt ->
+                stmt.setString(1, code)
+                stmt.executeQuery().use { rs ->
+                    if (rs.next()) {
+                        Pair(rs.getString("minecraft_uuid"), rs.getString("minecraft_name"))
+                    } else {
+                        null
+                    }
                 }
             }
-        }
         if (uuidName == null) {
-            defer.respond { content = "コードが無効です。" }
+            defer.edit { content = "コードが無効です。" }
             return
         }
-        val discordIdAndExpiresAt = DatabaseManager.query("SELECT `discord_id`, `expires_at` FROM `users` WHERE `link_code` = ?") { stmt ->
-            stmt.setString(1, code)
-            stmt.executeQuery().use { rs ->
-                if (rs.next()) {
-                    Pair(rs.getLong("discord_id"), rs.getLong("expires_at"))
-                } else {
-                    Pair(-1L, 0L)
+        val discordIdAndExpiresAt =
+            DatabaseManager.query("SELECT `discord_id`, `expires_at` FROM `users` WHERE `link_code` = ?") { stmt ->
+                stmt.setString(1, code)
+                stmt.executeQuery().use { rs ->
+                    if (rs.next()) {
+                        Pair(rs.getLong("discord_id"), rs.getLong("expires_at"))
+                    } else {
+                        Pair(-1L, 0L)
+                    }
                 }
             }
-        }
         if (discordIdAndExpiresAt.first != -1L || discordIdAndExpiresAt.second < System.currentTimeMillis()) {
             // already linked or expired
-            defer.respond { content = "コードが無効です。" }
+            defer.edit { content = "コードが無効です。" }
             return
         }
         val (uuid, name) = uuidName
         DatabaseManager.query("UPDATE `users` SET `discord_id` = ? WHERE `link_code` = ?") { stmt ->
-            stmt.setLong(1, interaction.user.id.value.toLong())
+            stmt.setLong(
+                1,
+                interaction.user.id.value
+                    .toLong(),
+            )
             stmt.setString(2, code)
             stmt.executeUpdate()
         }
-        DatabaseManager.getMinecraftUUIDByDiscordId.forget(interaction.user.id.value.toLong())
-        defer.respond { content = "`$name` (`$uuid`)と連携しました。" }
+        DatabaseManager.getMinecraftUUIDByDiscordId.forget(
+            interaction.user.id.value
+                .toLong(),
+        )
+        defer.edit { content = "`$name` (`$uuid`)と連携しました。" }
     }
 
     override fun register(builder: GlobalMultiApplicationCommandBuilder) {
